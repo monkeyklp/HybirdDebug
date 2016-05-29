3a87d55eb95e341b2887ecd279a15dc69f0fc153
monkey <842026955@qq.com>
2016-05-29 15:08:21 +0800

    add ignore

--- app/src/main/java/com/example/pc/hybirddebug/MainActivity.java
+++ app/src/main/java/com/example/pc/hybirddebug/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.pc.hybirddebug;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebView = (WebView)findViewById(R.id.webView);
+        
     }
 }
